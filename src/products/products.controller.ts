13efb83d6593a1461ed7e61d38c548251ef8109f
gasa1296 <gabriel.asa.1296@outlook.es>
2022-08-28 19:56:30 -0400

    Basic post request created

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { CreateProductDTO } from './dto/product.dto';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  @Post('/create')
+  createPost(@Res() res, @Body() createProductDTO: CreateProductDTO) {
+    //console.log(createProductDTO);
+    return res.status(HttpStatus.OK).json({
+      message: 'received',
+    });
+  }
+}
