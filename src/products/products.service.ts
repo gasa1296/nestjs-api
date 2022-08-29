a2269826398b58b92996ce4cba742c2838aa417f
gasa1296 <gabriel.asa.1296@outlook.es>
2022-08-28 22:00:09 -0400

    Readonly Iproduct attr

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { IProduct } from './interfaces/product.interface';
 
 @Injectable()
 export class ProductsService {}
