beba6035bb808f6b659065c8f6f91e983d67a98e
gasa1296 <gabriel.asa.1296@outlook.es>
2022-08-28 23:36:44 -0400

    Added generic to Schema

--- src/products/schemas/products.schema.ts
+++ src/products/schemas/products.schema.ts
@@ -1,6 +1,7 @@
 import { Schema } from 'mongoose';
+import { IProduct } from '../interfaces/product.interface';
 
-export const ProductSchema = new Schema({
+export const ProductSchema = new Schema<IProduct>({
   name: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
