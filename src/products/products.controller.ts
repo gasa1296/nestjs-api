1aca35ce9cdb61f6fed801b2a721455bbca90b1b
gasa1296 <gabriel.asa.1296@outlook.es>
2022-08-28 23:43:08 -0400

    Prettier corrections

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post, Put, Param, Delete } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { CreateProductDTO, UpdateProductDTO } from './dto/product.dto';
 import { ProductsService } from './products.service';
 
