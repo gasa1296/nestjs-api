c2b4b07de1bdfe5c76c1fc05e3a671e2bb61f6c9
gasa1296 <gabriel.asa.1296@outlook.es>
2022-08-28 21:57:00 -0400

    Added Schema & model

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductSchema } from './schemas/products.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Products', schema: ProductSchema }]),
+  ],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
