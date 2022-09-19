0c751bab91040decc1c04e646be2c5d512d7d1db
gasa1296 <gabriel.asa.1296@outlook.es>
2022-09-18 21:07:02 -0400

    default role

--- src/users/shemas/users.shema.ts
+++ src/users/shemas/users.shema.ts
@@ -4,7 +4,7 @@ import { IUser } from '../interfaces/user.interface';
 export const UserSchema = new Schema<IUser>({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: Number, required: true },
+  role: { type: Number, default: 1 },
   createdAt: {
     type: Date,
     default: Date.now,
